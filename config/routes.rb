9a3554bd464afcc54ef950a1e9df631e8d6315ed
eri-Q <eri.hatanaka8@gmail.com>
2021-12-17 14:51:14 +0000

    [Change]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root to: 'homes#top'
+  get "home/about" => "homes#about"
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  
+
 end
