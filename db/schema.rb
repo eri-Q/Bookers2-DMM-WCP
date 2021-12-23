e854a97ca95b48883273cc45f98bc02cfb5c126d
eri-Q <eri.hatanaka8@gmail.com>
2021-12-23 08:46:06 +0000

    [Add]4-10

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_055422) do
+ActiveRecord::Schema.define(version: 2021_12_23_065905) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
